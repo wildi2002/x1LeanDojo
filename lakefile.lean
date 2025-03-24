9ac69e58c5771ff30713143c1d4ca3f1f425dd86
Timo Wild <78898283+wildi2002@users.noreply.github.com>
2025-03-24 08:49:45 +0100

    Create lakefile.lean

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «lean4-example» {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_lib «Lean4Example» {
+  -- add library configuration options here
+}
