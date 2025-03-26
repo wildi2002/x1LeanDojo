24b591fd9677c22e8fe93a07b853b0b80dd1c003
Timo Wild <78898283+wildi2002@users.noreply.github.com>
2025-03-26 07:28:46 +0100

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -5,12 +5,10 @@ package «x1LeanDojo» {
   -- Hier können Build-Optionen definiert werden
 }
 
-lean_lib «Lean4Example» {
-  -- Hier können Bibliotheksoptionen stehen
-}
-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "master"
 
 @[default_target]
-lean_lib «Lean4Example»
+lean_lib «Lean4Example» {
+  -- Hier können Bibliotheksoptionen stehen
+}
