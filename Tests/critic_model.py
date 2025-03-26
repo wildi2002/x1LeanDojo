e1d178182e218f87e5341d1bf3ad43064c692c7a
tiwild <tiwild@student.ethz.ch>
2025-03-26 10:35:13 +0100

    c

---
+++ Tests/critic_model.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import AutoModel, AutoTokenizer
+
+model = AutoModel.from_pretrained(
+    "internlm/internlm2_5-step-prover-critic", 
+    device_map="cuda", 
+    torch_dtype=torch.float16, 
+    trust_remote_code=True,
+)
+tokenizer = AutoTokenizer.from_pretrained("internlm/internlm2_5-step-prover-critic", trust_remote_code=True)
+
+chat_1 = [
+    {"role": "user", "content": "Which state is closer to 'no goals'?"},
+    {"role": "assistant", "content": "no goals"}
+]
+chat_2 = [
+    {"role": "user", "content": "Which state is closer to 'no goals'?"},
+    {"role": "assistant", "content": "x : ℕ\nh₀ : ↑x + 4 / 100 * ↑x = 598\n⊢ 100 * x = 100 * 575"}
+]
+
+score1 = model.get_score(tokenizer, chat_1)
+score2 = model.get_score(tokenizer, chat_2)
+print("score1: ", score1)
+print("score2: ", score2)
