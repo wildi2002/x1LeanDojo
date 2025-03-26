594b1530d3e76baae63115162ca50884090bdd55
Timo Wild <78898283+wildi2002@users.noreply.github.com>
2025-03-26 07:27:55 +0100

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -2,13 +2,15 @@ import Lake
 open Lake DSL
 
 package «x1LeanDojo» {
-  -- add package configuration options here
+  -- Hier können Build-Optionen definiert werden
+}
+
+lean_lib «Lean4Example» {
+  -- Hier können Bibliotheksoptionen stehen
 }
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "master"
 
 @[default_target]
-lean_lib «Lean4Example» {
-  -- add library configuration options here
-}
+lean_lib «Lean4Example»
